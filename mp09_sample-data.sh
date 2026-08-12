0282165acda66d1f465ffa4b54d7bfdc557aefe0
Clara <88279664+claraweber@users.noreply.github.com>
2026-08-12 21:55:33 +0200

    some name stuff + cortical registration

--- mp09_sample-data.sh
+++ mp09_sample-data.sh
@@ -18,31 +18,41 @@ else
 fi
 outpth="outputs/equivols"
 
-# create tmp directory
-mkdir tmp
+# create tmp_${subj} directory
+mkdir "tmp_${subj}"
+
+# get mri ribbon
+mri_convert "${SUBJECTS_DIR}/${subj}/mri/aseg.mgz" "tmp_${subj}/aseg.nii.gz"
+fslmaths "tmp_${subj}/aseg.nii.gz" -thr 3 -uthr 3 -bin "tmp_${subj}/lhctx.nii.gz"
+fslmaths "tmp_${subj}/aseg.nii.gz" -thr 42 -uthr 42 -bin "tmp_${subj}/rhctx.nii.gz"
+fslmaths "tmp_${subj}/lhctx.nii.gz" -add "tmp_${subj}/rhctx.nii.gz" -bin "tmp_${subj}/maskctx.nii.gz"
+mri_convert "tmp_${subj}/maskctx.nii.gz" "tmp_${subj}/mask.mgz"
 
 # convert subject's MTsat to mgz
-mri_convert $MT_map "./tmp/${subj}-mt.mgz"
-mri_convert $R2_map "./tmp/${subj}-r2.mgz"
+mri_convert $MT_map "./tmp_${subj}/${subj}-mt_full.mgz"
+mri_convert $R2_map "./tmp_${subj}/${subj}-r2_full.mgz"
+
+mri_mask "./tmp_${subj}/${subj}-mt_full.mgz" "./tmp_${subj}/mask.mgz" "./tmp_${subj}/${subj}-mt.mgz"
+mri_mask "./tmp_${subj}/${subj}-r2_full.mgz" "./tmp_${subj}/mask.mgz" "./tmp_${subj}/${subj}-r2.mgz"
 
 for hemi in lh rh; do
 	for depth in 0.0 0.07692307692307693 0.15384615384615385 0.23076923076923078 0.3076923076923077 0.38461538461538464 0.46153846153846156 0.5384615384615384 0.6153846153846154 0.6923076923076923 0.7692307692307693 0.8461538461538461  0.9230769230769231 1.0; do
 	# sample at surface
-	mri_vol2surf --interp trilin --src "./tmp/${subj}-mt.mgz" --out "./tmp/${subj}_${hemi}_${depth}.mgh" --hemi $hemi --surf "equi${depth}.pial" --regheader $subj
+	mri_vol2surf --interp trilin --src "./tmp_${subj}/${subj}-mt.mgz" --out "./tmp_${subj}/${subj}_${hemi}_${depth}.mgh" --hemi $hemi --surf "equi${depth}.pial" --regheader $subj
 
 	# resample to fsa5
-	mri_surf2surf --srcsubject $subj --srcsurfval "./tmp/${subj}_${hemi}_${depth}.mgh" --trgsubject ico --trgicoorder 5 --trgsurfval "${outpth}/${subj}_${depth}_fsa5.${hemi}.mgh" --hemi $hemi #--nsmooth-out 5
+	mri_surf2surf --srcsubject $subj --srcsurfval "./tmp_${subj}/${subj}_${hemi}_${depth}.mgh" --trgsubject ico --trgicoorder 5 --trgsurfval "${outpth}/${subj}_${depth}_fsa5.${hemi}.mgh" --hemi $hemi #--nsmooth-out 5
 	
 	# R2s
-	mri_vol2surf --interp trilin --src "./tmp/${subj}-r2.mgz" --out "./tmp/${subj}_${hemi}_${depth}_r2.mgh" --hemi $hemi --surf "equi${depth}.pial" --regheader $subj
+	mri_vol2surf --interp trilin --src "./tmp_${subj}/${subj}-r2.mgz" --out "./tmp_${subj}/${subj}_${hemi}_${depth}_r2.mgh" --hemi $hemi --surf "equi${depth}.pial" --regheader $subj
 
 	# resample to fsa5
 	mri_surf2surf --srcsubject $subj --srcsurfval "./tmp/${subj}_${hemi}_${depth}_r2.mgh" --trgsubject ico --trgicoorder 5 --trgsurfval "${outpth}/${subj}_${depth}_r2_fsa5.${hemi}.mgh" --hemi $hemi #--nsmoothout 5
 	done
 done
 
-# clear tmp directory
-rm -r tmp
+# clear tmp_${subj} directory
+rm -r tmp_${subj}
 
 # get all data for subject in the same df
 python mp09_helper_aggregate.py ${1}
